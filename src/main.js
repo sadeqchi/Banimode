965bc50fe16fde4556d61d850ab2d1f2831dce96
Sadeq Eslah <sadeqeslah@gmail.com>
2026-02-28 00:46:18 +0330

    Adding internal API for amazing section

--- src/main.js
+++ src/main.js
@@ -11,10 +11,8 @@ var swiper = new Swiper(".mySwiper_1", {
     disableOnInteraction: false,
   },
 });
-var swiper = new Swiper(".mySwiper_2", {
-slidesPerView: 5,
-spaceBetween: 30,
-});
+
+
 var swiper = new Swiper(".mySwiper_3", {
 slidesPerView: 1,
 spaceBetween: 10,
@@ -35,3 +33,35 @@ var swiper = new Swiper(".mySwiper_5", {
         clickable: true,
       },
     });
+    
+const swiperOne = document.getElementById("swiperOne");
+const endPointOne = "http://localhost:3000/swiperWonder";
+fetch(endPointOne,Option)
+.then((res) => res.json())
+.then((api) => {
+  const swiperOneApi = api.map ((items) => items )
+  swiperOneApi.forEach( Element => {
+    swiperOne.insertAdjacentHTML("beforeend",
+      `
+        <div id="${Element.id}" class="swiper-slide">
+          <div class="swipercard">
+            <img class="w-full h-full object-contain" src="${Element.image}" alt="">
+            <p class="text-center text-sm line-clamp-1 font-extrabold p-5">${Element.title}</p>
+            <div class="flex justify-between items-center p-2">
+              <span class="text-stone-500"><s>${Element.offer}</s></span>
+              <span class="flex justify-center items-center gap-x-1">${Element.price}<img class="w-5 h-5" src="../image/tomanLogo.png" alt=""></span>
+            </div>        
+          </div>
+        </div>
+        `
+    )
+    var swiper = new Swiper(".mySwiper_2", {
+    slidesPerView: 5,
+    spaceBetween: 20,
+    });
+  });
+});
+        
+
+
+   
